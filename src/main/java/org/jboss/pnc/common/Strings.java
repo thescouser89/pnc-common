150b31816e9ced6580b1161c2b1fa385431a02db
Matej Lazar <matejonnet@gmail.com>
2022-06-27 11:01:21 +0200

    Add Strings.fistCharToLower method.

--- src/main/java/org/jboss/pnc/common/Strings.java
+++ src/main/java/org/jboss/pnc/common/Strings.java
@@ -111,4 +111,10 @@ public class Strings {
             return defaultValue;
         }
     }
+
+    public static String fistCharToLower(String string) {
+        char c[] = string.toCharArray();
+        c[0] = Character.toLowerCase(c[0]);
+        return new String(c);
+    }
 }
